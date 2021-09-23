d6fefca500755661e050b6893a7cae9acb905da4
Julio Guzman <julio.guzman@me.com>
2021-09-22 20:51:28 -0700

    Hacer que el control funcione

--- ControllerTest/GameViewController.swift
+++ ControllerTest/GameViewController.swift
@@ -1,16 +1,41 @@
 //
 //  GameViewController.swift
-//  ControllerTest
+//  Videojuego
 //
-//  Created by Julio Cesar Guzman Villanueva on 9/22/21.
+//  Created by Julio Cesar Guzman Villanueva on 9/7/21.
 //
 
 import UIKit
 import SpriteKit
 import GameplayKit
+import GameController
 
 class GameViewController: UIViewController {
 
+    private lazy var gameController : GCVirtualController = {
+        var config = GameController.GCVirtualController.Configuration.init()
+        config.elements = [
+            GCInputButtonA,
+            GCInputButtonB,
+            GCInputButtonX,
+            GCInputButtonY,
+            GCInputDirectionPad,
+            GCInputLeftThumbstick,
+            GCInputRightThumbstick,
+            GCInputLeftShoulder,
+            GCInputRightShoulder,
+            GCInputLeftTrigger
+        ]
+        let gameController = GameController.GCVirtualController.init(configuration: config)
+        gameController.connect { error in
+            print(error as Any)
+        }
+        
+        
+        
+        return gameController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,12 +46,8 @@ class GameViewController: UIViewController {
             // Get the SKScene from the loaded GKScene
             if let sceneNode = scene.rootNode as! GameScene? {
                 
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
+                registerGameController(gameController)
                 
                 // Present the scene
                 if let view = self.view as! SKView? {
@@ -36,6 +57,7 @@ class GameViewController: UIViewController {
                     
                     view.showsFPS = true
                     view.showsNodeCount = true
+                    view.presentScene(sceneNode)
                 }
             }
         }
@@ -55,5 +77,34 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    func registerGameController(_ gameController: GCVirtualController) {
+        let buttonA = gameController.controller?.microGamepad?.buttonA
+        let b = gameController.controller?.microGamepad?.buttonX
+        
+        weak var weakController = self
+        
+        buttonA?.valueChangedHandler = {(_ button: GCControllerButtonInput, _ value: Float, _ pressed: Bool) -> Void in
+            guard let strongController = weakController else {
+                return
+            }
+            strongController.controllerJump(pressed)
+        }
+
+        b?.valueChangedHandler = {(_ button: GCControllerButtonInput, _ value: Float, _ pressed: Bool) -> Void in
+            guard let strongController = weakController else {
+                return
+            }
+            strongController.controllerAttack()
+        }
+    }
+    
+    func controllerAttack() {
+        print("Attack")
+    }
+    
+    func controllerJump(_ pressed: Bool) {
+        print(pressed)
     }
 }
